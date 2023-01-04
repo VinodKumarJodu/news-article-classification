bc993b8e27ac602c7a4f79e68500a3f7311dd734
VinodKumarJodu <vinodkumarjodu@gmail.com>
2023-01-04 19:30:43 +0530

    data_transformation component created

---
+++ news/components/data_transformation.py
@@ -0,0 +1,18 @@
+import os, sys
+import numpy as np 
+import pandas as pd 
+
+from news.logger import logging
+from news.exception import NewsException
+
+from news.entity.config_entity import DataTransformationConfig
+from news.entity.artifact_entity import DataTransformationConfig
+
+class DataTransformation:
+    def __init__(self, data_transformation_config: DataTransformationConfig):
+        try:
+            self.data_transformation_config = data_transformation_config
+             
+        except Exception as e:
+            raise NewsException(e, sys)
+
