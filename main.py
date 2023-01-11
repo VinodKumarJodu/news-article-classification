ebce385537d1291067d36795f6da418221e08f69
VinodKumarJodu <vinodkumarjodu@gmail.com>
2023-01-11 09:15:06 +0530

    Updated Code base

--- main.py
+++ main.py
@@ -15,58 +15,52 @@ from nltk.stem import WordNetLemmatizer
 from gensim.models import word2vec
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-from sklearn.preprocessing import LabeEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
-from sklearn.metrics import log_loss
+from sklearn.metrics import log_loss, confusion_matrix, accuracy
 from sklearn.pipeline import Pipeline
 
 import joblib
 
 import warnings
 warnings.filterwarnings('ignore')
-nltk.download('stopwords')
+# nltk.download('stopwords')
 
 stop = stopwords.words('english')
 
+path = '/config/workspace/source/train/train.csv'
+def read_data(path):
+    df = pd.read_csv(path, names=['articleid', 'text', 'category'])
+    # df.columns = [column.lower() for column in df.columns]
+    return df
+
 def preprocess(text):
-    # text to lower case
     text = " ".join([word.lower() for word in text.split()])
-
-    # remove url
     text = re.sub(r"\S*https?:\S*",'',text)
-
-    # remove html tags
     text = re.sub(r"<.*?>",'',text)
-
-    # remove punctuations
     text = re.sub('[%s]' %re.escape(string.punctuation), '', text)
-
-    #remove new line character
     text = re.sub(r'\n','',text)
-
-    # remove stopwords
     text = " ".join([word for word in text.split() if word not in stopwords.words('english')])
-
     return text
 
-def split_data(df):
-    df.columns = [column.lower() for column in df.columns]
+def split_data(X,y):
+    # df.columns = [column.lower() for column in df.columns]
     X = df['text']
     y = df['category']
-    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=19, stratify=True)
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=19)
+    vectorizer = TfidfVectorizer(stop_words='english')
+    X_train = vectorizer.fit_transform(X_train)
+    X_test = vectorizer.transform(X_test)
     return X_train, X_test, y_train, y_test
 
-def label_encode_y(y):
-    le = LabeEncoder()
-    return le.fit_transform(y)
-
-def vectorize(df, column):
-    vectorizer = TfidfVectorizer(stop_words='english')
-    df = vectorizer.fit_transform(df[colunm])
-    return df    
+def label_encode(y_train,y_test):
+    le = LabelEncoder()
+    y_train = le.fit_transform(y_train)
+    y_test = le.transform(y_test)
+    return y_train, y_test
 
 def model_training(X_train,y_train, X_test, y_test):
     model = MultinomialNB()
@@ -74,4 +68,21 @@ def model_training(X_train,y_train, X_test, y_test):
     y_pred = model.predict(X_test)
     return log_loss(y_test.values, y_pred)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
+    df = read_data(path)
+    print(df.columns)
+    df['text'] = df['text'].apply(lambda x:preprocess(x))
+    X = df['text']
+    y = df['category']
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=19, stratify=True)
+    vectorizer = TfidfVectorizer(stop_words='english')
+    X_train = vectorize.fit_transform(X_train)
+    X_test = vectorizer.transform(X_test)
+    le = LabelEncoder()
+    y_train = le.fit_transform(y_train)
+    y_test = le.transitionType(y_test)
+    model = MultinomialNB()
+    model = model.fit(X_train.to_array(),y_train.values)
+    y_pred = model.predict(X_test)
+    loss = log_loss(y_test,y_pred)
+    print(loss)
