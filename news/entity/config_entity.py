0ac7591201107f3b8b11a7f0d2d6b345713b334d
VinodKumarJodu <vinodkumarjodu@gmail.com>
2023-02-04 21:01:36 +0530

    Built Data Ingestion Pipeline

--- news/entity/config_entity.py
+++ news/entity/config_entity.py
@@ -10,19 +10,33 @@ from news.constants.training_pipeline_constants import PIPELINE_NAME, ARTIFACTS_
 from news.constants.training_pipeline_constants import DATA_INGESTION_DIR_NAME, DATA_INGESTION_FEATURE_STORE_DIR, DATA_INGESTION_INGESTED_DIR, DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
 
 class TrainingPipelineConfig:
-    def __int__(self, timestamp=datetime.now()):
-        timestamp = timestamp.strftime('%m_%d_%Y_%H_%M_%S')
+
+    def __init__(self,timestamp=datetime.now()):
+        timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name: str = PIPELINE_NAME
         self.artifact_dir: str = os.path.join(ARTIFACTS_DIR, timestamp)
         # self.timestamp: str = timestamp
 
+
 class DataIngestionConfig:
-    def __init__(self, training_pipeline_config:TrainingPipelineConfig):
-        self.data_ingestion_dir:str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)
-        self.feature_store_file_path:str = os.path.join(self.data_ingestion_dir,DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
-        self.train_file_path: str = os.path.join(self.data_ingestion_dir,DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
-        self.test_file_path: str = os.path.join(self.data_ingestion_dir,DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
-        self.train_test_split_ration: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+        def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+            self.data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)
+            self.feature_store_file_path: str = os.path.join(self.data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
+            self.train_file_path: str = os.path.join(self.data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+            self.test_file_path: str = os.path.join(self.data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+            self.train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+
+
+
+
+# class DataIngestionConfig:
+#     def __init__(self, training_pipeline_config:TrainingPipelineConfig):
+#         self.training_pipeline_config = training_pipeline_config
+#         self.data_ingestion_dir:str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)
+#         self.feature_store_file_path:str = os.path.join(self.data_ingestion_dir,DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
+#         self.train_file_path: str = os.path.join(self.data_ingestion_dir,DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+#         self.test_file_path: str = os.path.join(self.data_ingestion_dir,DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+#         self.train_test_split_ration: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
 
 # class DataTransformationConfig:
 #     def __init__(self, training_pipeline_config:TrainingPipelineConfig):
