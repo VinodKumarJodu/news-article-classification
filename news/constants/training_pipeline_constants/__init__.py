7c2632e99a4a6fd2ee850ed3f6948d3a33462369
VinodKumarJodu <vinodkumarjodu@gmail.com>
2023-02-04 21:34:42 +0530

    Defined Data Validation Related Constants

--- news/constants/training_pipeline_constants/__init__.py
+++ news/constants/training_pipeline_constants/__init__.py
@@ -19,6 +19,15 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
+
+"""
+Defining Data Validation Related Constants:: DATA_VALIDATION
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+
+
 """
 Defining Data Transformation Related Constants & Starts with DATA_TRANSFORMATION 
 """
