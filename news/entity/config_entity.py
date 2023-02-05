2150f56c2359058bbb3537e924126fccf5d97c6a
VinodKumarJodu <vinodkumarjodu@gmail.com>
2023-02-05 19:04:55 +0530

    Data Validation Configurations created

--- news/entity/config_entity.py
+++ news/entity/config_entity.py
@@ -9,7 +9,7 @@ from news.constants.training_pipeline_constants import PIPELINE_NAME, ARTIFACTS_
 #Data Ingestion related Constants
 from news.constants.training_pipeline_constants import DATA_INGESTION_DIR_NAME, DATA_INGESTION_FEATURE_STORE_DIR, DATA_INGESTION_INGESTED_DIR, DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
 #Data Validation Relaed Constants
-from news.constants.training_pipeline_constants import DATA_VALIDATION_DIR_NAME, DATA_VALIDATION_VALID_DIR, DATA_VALIDATION_INVALID_DIR
+from news.constants.training_pipeline_constants import DATA_VALIDATION_DIR_NAME, DATA_VALIDATION_VALID_DIR, DATA_VALIDATION_INVALID_DIR, DATA_VALIDATION_REPORT_DIR, DATA_VALIDATION_REPORT_FILE_NAME
 class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
@@ -29,13 +29,13 @@ class DataIngestionConfig:
 class DataValidationConfig:
     def __init__(self, training_pipeline_config:TrainingPipelineConfig):
         self.data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir,DATA_VALIDATION_DIR_NAME)
-        self.valid_data_dir:str = os.path.join(self.data_validation_dir, DATA_VALIDATION_VALID_DIR)
+        self.valid_data_dir: str = os.path.join(self.data_validation_dir, DATA_VALIDATION_VALID_DIR)
         self.invalid_data_dir: str = os.path.join(self.data_validation_dir, DATA_VALIDATION_INVALID_DIR)
         self.valid_train_file_path: str = os.path.join(self.valid_data_dir, TRAIN_FILE_NAME)
         self.valid_test_file_path: str = os.path.join(self.valid_data_dir, TEST_FILE_NAME)
         self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, TRAIN_FILE_NAME)
         self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir, TEST_FILE_NAME)
-
+        self.validation_report_path: str = os.path.join(self.data_validation_dir, DATA_VALIDATION_REPORT_DIR, DATA_VALIDATION_REPORT_FILE_NAME)
 
 # class DataIngestionConfig:
 #     def __init__(self, training_pipeline_config:TrainingPipelineConfig):
