9a9802beb1a4ffe6a8c3b67499a43143b0f61eae
VinodKumarJodu <vinodkumarjodu@gmail.com>
2023-02-05 19:04:30 +0530

    artifacts for data validation  created

--- news/entity/artifact_entity.py
+++ news/entity/artifact_entity.py
@@ -6,8 +6,15 @@ class DataIngestionArtifact:
     test_file_path: str
 
 @dataclass
+class ValidationResult:
+    validate_number_of_columns: bool
+    column_name_validation: bool
+    datatype_validation: bool
+    target_label_validation:bool
+
+@dataclass
 class DataValidationArtifact:
-    validation_status: bool
+    validation_results: ValidationResult
     valid_train_file_path: str
     valid_test_file_path: str
     invalid_train_file_path: str
