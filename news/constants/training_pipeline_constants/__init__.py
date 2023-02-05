6ee689350fe23ae914409cc162fdfa78f77c29ab
VinodKumarJodu <vinodkumarjodu@gmail.com>
2023-02-05 19:03:57 +0530

    Defined  Data Validation Constants

--- news/constants/training_pipeline_constants/__init__.py
+++ news/constants/training_pipeline_constants/__init__.py
@@ -26,6 +26,8 @@ Defining Data Validation Related Constants:: DATA_VALIDATION
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_REPORT_DIR: str = "validation_results"
+DATA_VALIDATION_REPORT_FILE_NAME: str = "data_validation_report.yaml"
 
 
 """
